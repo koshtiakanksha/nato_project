239856d9df264fd72e6ef5546bafe0b1175976b0
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-02-18 11:40:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import pandas
 
+alphabets = False
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-name = input("Please enter the word\n").upper()
-output_list = [phonetic_dict[i] for i in name]
-print(output_list)
+while not alphabets:
+    name = input("Please enter the word:\n").upper()
+    alphabets = name.isalpha()
+    try:
+        output_list = [phonetic_dict[i] for i in name]
+    except KeyError:
+        print("Please enter alphabets only")
+    else:
+        print(output_list)
